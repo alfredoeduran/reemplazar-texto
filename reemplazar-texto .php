5f5b6af771e46e774101137c2246859573245c8c
Alfredo Duran <alfredoeduran@gmail.com>
2024-05-22 19:03:27 -0500

    Actualizacion v1.1
    se añaden nuevos terminos

--- reemplazar-texto .php
+++ reemplazar-texto .php
@@ -1,12 +1,18 @@
 <?php
 /*
 Plugin Name: Reemplazar Texto
-Description: Reemplaza la palabra "noche" por "persona" en todo el sitio.
-Version: 1.0
-Author: AlfredDuran
+Description: Reemplaza la palabra "noche" por "persona" y "por noche" por "por persona" en todo el sitio.
+Version: 1.1
+Author: Tu Nombre
 */
+
 function reemplazar_texto_noche_persona($content) {
+    // Reemplazar "noche" por "persona"
     $nuevo_contenido = str_replace('noche', 'persona', $content);
+
+    // Reemplazar "por noche" por "por persona"
+    $nuevo_contenido = str_replace('por noche', 'por persona', $nuevo_contenido);
+
     return $nuevo_contenido;
 }
 add_filter('the_content', 'reemplazar_texto_noche_persona');
